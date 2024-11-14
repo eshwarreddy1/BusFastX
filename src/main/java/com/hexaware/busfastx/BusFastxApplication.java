862f163eb847189ea0c3b2a532876edf6e929510
eshwarreddy1 <152689926+eshwarreddy1@users.noreply.github.com>
2024-11-14 21:36:20 +0530

    commit

---
+++ src/main/java/com/hexaware/busfastx/BusFastxApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.busfastx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusFastxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusFastxApplication.class, args);
+	}
+
+}
